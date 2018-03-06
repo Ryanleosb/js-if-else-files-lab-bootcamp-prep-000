0783ebba207c03f0a9f3df51054b60c49b1379fa
leo bang <ryanleosb@gmail.com>
2018-03-06 01:08:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (name === "Joe") {
 } else {
   console.log("Expected the name to be Joe")
 }
-if (height ==="74"){
+if (index.height ==="74"){
   console.log("The hight is correct")
 } else {
   console.log("Expected the height to be 74")
@@ -16,7 +16,7 @@ if (height ==="74"){
 if (name === "Susan") {
   console.log("The name is correct")
 }
-if (height ==="70"){
+if (index.height ==="70"){
   console.log("The height is correct")
 } else {
   console.log("Expected the height to be correct")
