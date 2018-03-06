f5d835d7040fe65280277a35fa22ebea6bd778ed
leo bang <ryanleosb@gmail.com>
2018-03-06 00:45:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ var height = "70"
 
 if (name === "Joe") {
   console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Joe")
 }
 if (height ==="74"){
   console.log("The hight is correct")
-} else {
-  console.log("Expected the height to be 74")
+}
 }
 if (name === "Susan") {
   console.log("The name is correct")
